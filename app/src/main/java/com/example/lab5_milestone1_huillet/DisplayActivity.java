5b2cd535f5e6f4976d6d5c5f2206c42a1689f2fe
Levi Huillet <levimhuillet@gmail.com>
2021-10-16 14:36:25 -0500

    second screen is built

--- app/src/main/java/com/example/lab5_milestone1_huillet/DisplayActivity.java
+++ app/src/main/java/com/example/lab5_milestone1_huillet/DisplayActivity.java
@@ -1,9 +1,14 @@
 package com.example.lab5_milestone1_huillet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
@@ -17,5 +22,32 @@ public class DisplayActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
         welcomeText.setText("Welcome " + str + "!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.display_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                goToMainActivity();
+                break;
+            case R.id.add:
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void goToMainActivity()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
